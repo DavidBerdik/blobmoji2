e24bb868d973d6f641a5d5bece076ff2ed8c2886
David Berdik <berdikgit@theprogrammersworld.net>
2025-03-26 20:36:51 -0400

    Remove build and png directories as mountpoints for building with Docker.

--- docker_build.sh
+++ docker_build.sh
@@ -1,1 +1,1 @@
-docker build . -t blobmoji && docker run --rm -it -v "$PWD/build:/blobmoji/build" -v "$PWD/png:/blobmoji/png" -v "$PWD/fonts:/blobmoji/fonts" blobmoji
+docker build . -t blobmoji && docker run --rm -it -v "$PWD/fonts:/blobmoji/fonts" blobmoji
