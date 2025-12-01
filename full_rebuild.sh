b191e5414144072bde89821796f59dfb61d283c8
David Berdik <berdikgit@theprogrammersworld.net>
2025-12-01 15:19:33 -0500

    Add experimental support for building COLRv0 variant of blobmojis.

--- full_rebuild.sh
+++ full_rebuild.sh
@@ -46,16 +46,33 @@ cp colrv1/build/NotoColorEmoji-noflags.ttf fonts/Noto-COLRv1-noflags.ttf
 # Post-process them
 python colrv1_postproc.py
 
+# Build the COLRv0 font (slow)
+
+python colrv0_generate_configs.py
+git diff colrv0/*.toml
+
+# Compile the fonts
+# Should take ~20 minutes
+(cd colrv0 && rm -rf build/ && time nanoemoji *.toml)
+cp colrv0/build/NotoColorEmoji.ttf fonts/Noto-COLRv0.ttf
+cp colrv0/build/NotoColorEmoji-noflags.ttf fonts/Noto-COLRv0-noflags.ttf
+
+# Post-process them
+python colrv0_postproc.py
+
 # Produce emojicompat variants
 # Add support for new sequences per https://github.com/googlefonts/emojicompat#support-new-unicode-sequences
 
 pushd fonts
 cp NotoColorEmoji.ttf NotoColorEmoji-emojicompat.ttf
 cp Noto-COLRv1.ttf Noto-COLRv1-emojicompat.ttf
+cp Noto-COLRv0.ttf Noto-COLRv0-emojicompat.ttf
 emojicompat --op setup --font NotoColorEmoji-emojicompat.ttf
 emojicompat --op setup --font Noto-COLRv1-emojicompat.ttf
+emojicompat --op setup --font Noto-COLRv0-emojicompat.ttf
 emojicompat --op check --font NotoColorEmoji-emojicompat.ttf
 emojicompat --op check --font Noto-COLRv1-emojicompat.ttf
+emojicompat --op check --font Noto-COLRv0-emojicompat.ttf
 popd
 
 hb-subset --unicodes-file=flags-only-unicodes.txt \
